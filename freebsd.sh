de6cd843eab9d0b636861d1d633a6338eaf06ede
Vince <65380728+Socerest2@users.noreply.github.com>
2021-02-28 00:34:29 -0500

    Create freebsd.sh

---
+++ freebsd.sh
@@ -0,0 +1,4 @@
+sudo pkg update && sudo pkg install -g libidn2 ca_root_nss
+sudo pkg add "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-freebsd.pkg"
+sudo pkg update && sudo pkg install -g libidn2 ca_root_nss
+sudo pkg add "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-freebsd.pkg"
